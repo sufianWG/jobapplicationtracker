6039a43be1e8de12cff06668a87a93995bafed4c
sufianWG <webgurumrmbd@gmail.com>
2026-02-22 13:11:17 +0600

    toogle style function created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,8 @@ const filteredSection = document.getElementById("filtered-section");
 const mainContainer = document.querySelector("main");
 
 const availableJobs = document.getElementById("available-jobs");
-
+const noJobAvailableSection = document.getElementById("no-job-available");
+// console.log(noJobAvailableSection);
 // console.log( "total Count:", totalCountElement.innerText, "InterView Count:", interViewElement.innerText, "RejectedCount:", rejectedElement.innerText)
 // console.log( "Toggler All btn:", allTogglerBtn.innerText, "|", "InterView Toggler Btn:", InterViewTogglerBtn.innerText, "|", "Rejected Toggler Btn:", rejectedTogglerBtn.innerText)
 // console.log( "All Job Card Data:", allJobCards.innerText, "|", "Filter Section Data:", filteredSection.innerText, "|", "Main Container Data:", mainContainer.innerText)
@@ -21,6 +22,7 @@ const availableJobs = document.getElementById("available-jobs");
 let totalCountList = [];
 let interViewList = [];
 let rejectedList = [];
+let currentStatus = 'all';
 
 // console.log(allJobCards.children.length) ;
 
@@ -29,3 +31,22 @@ function calculateCount(){
     availableJobs.innerText = allJobCards.children.length
 }
 calculateCount()
+
+function toggler(id){
+    // currentStatus = id;
+    // console.log(id);
+    allTogglerBtn.classList.add('bg-white', 'text-gray-700');
+    InterViewTogglerBtn.classList.add('bg-white', 'text-gray-700');
+    rejectedTogglerBtn.classList.add('bg-white', 'text-gray-700');
+
+    allTogglerBtn.classList.remove('bg-blue-600', 'text-white');
+    InterViewTogglerBtn.classList.remove('bg-blue-600', 'text-white');
+    rejectedTogglerBtn.classList.remove('bg-blue-600', 'text-white');
+
+    const selected = document.getElementById(id);
+    selected.classList.remove('bg-white', 'text-gray-700');
+    selected.classList.add('bg-blue-600', 'text-white');
+
+    
+
+}
